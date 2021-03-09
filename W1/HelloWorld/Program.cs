e7560c904010c15d911c2f56ac787707f2b6e7b4
csfplus <dlucsf@gmail.com>
2021-03-10 01:47:52 +0800

    添加项目文件。

---
+++ W1/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            string s = Console.ReadLine();
+            Console.WriteLine($"你输入的是:{s}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ W1/MyFirstWinFormsApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+            this.Text = "你未满18岁！";
+            MessageBox.Show("密码错误!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("我也喜欢你");
+        }
+
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            button3.Location = new Point(r.Next(this.ClientSize.Width-button3.Width), r.Next(ClientSize.Height-button3.Height));
+        }
+    }
+}
